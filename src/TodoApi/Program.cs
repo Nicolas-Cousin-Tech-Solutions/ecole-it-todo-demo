addf3d46fb614a5b5fb2235ff5a7fdd5f2be71d7
Nicolas Cousin <contact@nicolas-cousin.com>
2025-04-01 13:31:51 +0000

    Ajout du context dans l'API

--- src/TodoApi/Program.cs
+++ src/TodoApi/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<TodoContext>(options =>
+    options.UseInMemoryDatabase("TodoList"));
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
