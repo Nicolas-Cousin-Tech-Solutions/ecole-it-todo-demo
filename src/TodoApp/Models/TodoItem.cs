cd8edd26142dc74fe5a855f5bb775e375acc97a0
Nicolas Cousin <contact@nicolas-cousin.com>
2025-04-01 22:02:45 +0000

    Ajoute la page pour une nouvelle tache

--- src/TodoApp/Models/TodoItem.cs
+++ src/TodoApp/Models/TodoItem.cs
@@ -1,3 +1,8 @@
 namespace TodoApp.Models;
 
-public record TodoItem(int Id, string Title, bool IsDone = false);
+public class TodoItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public bool IdDone { get; set; } = false;
+}    
